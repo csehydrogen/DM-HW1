f83e804a4488910265e740f8d08bc84ee4d5489e
HeeHoon Kim <csehydrogen@gmail.com>
2016-03-18 14:54:32 +0900

    DegreeDistribution implemented
    same as IndegreeCounter

--- DegreeDistribution.java
+++ DegreeDistribution.java
@@ -1,1 +1,40 @@
-//fill your code
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.lib.input.KeyValueTextInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.reduce.IntSumReducer;
+
+import java.io.IOException;
+
+public class DegreeDistribution {
+    private static class MyMapper
+            extends Mapper<Text, Text, IntWritable, IntWritable> {
+        private final static IntWritable ONE = new IntWritable(1);
+        private IntWritable keyout = new IntWritable();
+        public void map(Text src, Text dst, Context context)
+                throws IOException, InterruptedException {
+            keyout.set(Integer.parseInt(dst.toString()));
+            context.write(keyout, ONE);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "degree count");
+        job.setJarByClass(DegreeDistribution.class);
+        job.setMapperClass(MyMapper.class);
+        job.setCombinerClass(IntSumReducer.class);
+        job.setReducerClass(IntSumReducer.class);
+        job.setInputFormatClass(KeyValueTextInputFormat.class);
+        job.setOutputKeyClass(IntWritable.class);
+        job.setOutputValueClass(IntWritable.class);
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
